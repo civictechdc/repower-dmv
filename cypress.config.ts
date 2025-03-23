5d563cb2960ad45b7f1e35cb5a1ff83500aa3c6a
Sheldon Bachstein <sheldon@civictechdc.org>
2025-03-23 10:35:33 -0400

    Try removing stuff to see if it works

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,16 +3,12 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents: (on, config) => {
-      console.log("1111111111111111111111");
-      const isDev = config.watchForFileChanges;
-      console.log("2222222222222222222222");
-      const port = process.env.PORT ?? (isDev ? "3000" : "8811");
-      console.log("3333333333333333333333");
-      const configOverrides: Partial<Cypress.PluginConfigOptions> = {
-        baseUrl: `http://localhost:${port}`,
-        screenshotOnRunFailure: !process.env.CI,
-      };
-      console.log("44444444444444444444444");
+      // const isDev = config.watchForFileChanges;
+      // const port = process.env.PORT ?? (isDev ? "3000" : "8811");
+      // const configOverrides: Partial<Cypress.PluginConfigOptions> = {
+      //   baseUrl: `http://localhost:${port}`,
+      //   screenshotOnRunFailure: !process.env.CI,
+      // };
 
       // To use this:
       // cy.task('log', whateverYouWantInTheTerminal)
@@ -24,7 +20,8 @@ export default defineConfig({
         },
       });
 
-      return { ...config, ...configOverrides };
+      // return { ...config, ...configOverrides };
+      return config;
     },
   },
 });
