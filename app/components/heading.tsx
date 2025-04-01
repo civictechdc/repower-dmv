0d6fafebcbdb02bdbea6dd5190d39c7544fc77f2
Tony Vitez <amvitez@gmail.com>
2025-03-31 21:53:45 -0400

    Requested changes from last PR
    Use children in heading component
    Add back in cy tests but skip them for now

--- app/components/heading.tsx
+++ app/components/heading.tsx
@@ -1,8 +1,15 @@
-export default function Heading({title} : {title: string}) {
+import { PropsWithChildren } from 'react'
+
+/* eslint-disable */
+// Disabling "no empty interface" rule
+interface HeadingProps { }
+/* eslint-enable */
+
+export default function Heading(props: PropsWithChildren<HeadingProps>) {      
 
   return (
     <h1 className="text-center text-4xl font-extrabold tracking-tight text-gray-900">
-        { title }
+        { props.children }
     </h1>
   );
 }
